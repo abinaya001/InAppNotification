71eebdfb165e517ba4c22fc14543d4e89f9d76c3
abinaya001 <abinaya.palaniswamy@gmail.com>
2016-10-18 17:45:47 +0530

    InAppNotificationPUsh

---
+++ InAppNotification/InAppNotification.h
@@ -0,0 +1,19 @@
+//
+//  InAppNotification.h
+//  InAppNotification
+//
+//  Created by Abinaya Palanisamy on 18/10/16.
+//  Copyright © 2016 Abinaya Palanisamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppNotification.
+FOUNDATION_EXPORT double InAppNotificationVersionNumber;
+
+//! Project version string for InAppNotification.
+FOUNDATION_EXPORT const unsigned char InAppNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppNotification/PublicHeader.h>
+
+
